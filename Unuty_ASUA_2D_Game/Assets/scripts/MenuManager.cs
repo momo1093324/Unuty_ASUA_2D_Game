2ed7a6c21175065ad64d31c97896cfb342751722
momo1093324 <71679878+momo1093324@users.noreply.github.com>
2020-11-03 16:04:06 +0800

    加入介面連結程式

--- Unuty_ASUA_2D_Game/Assets/scripts/MenuManager.cs
+++ Unuty_ASUA_2D_Game/Assets/scripts/MenuManager.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
 //按鈕如何與程式溝通
-void StartGame()
+//加入public使其公開，讓按鈕能與程式連結 
+public void StartGame() 
     {
-
+        print("開始遊戲");
+        SceneManager.LoadScene("關卡1");
+        //另一種的語法方式 SceneManager.LoadScene(1);
+        //皆指場景連結，可從選單進入關卡1；另外需把介面與關卡拉入unity build setting
     }
-    void QuitGame()
+  public void QuitGame()
     {
-
+        print("結束遊戲");
+        Application.Quit();    //遊戲退出，需發佈才會出現
     }
 }
---
+++ Unuty_ASUA_2D_Game/Assets/scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //欄位:儲存資料
+    public GameObject final1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+        //碰撞偵測條件
+        //1.兩者物件必須皆有碰撞器 collider2D  2.兩者其一需有一個剛體 Rigidboby2D 
+    {
+        print("遇到傳送奇遇時間");
+        final1.SetActive(true);      //啟動設定(是)
+    }
+
+}
