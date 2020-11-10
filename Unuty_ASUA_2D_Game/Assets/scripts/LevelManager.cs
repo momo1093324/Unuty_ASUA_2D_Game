f22d5586428884fa140ccd28511d829c6f83719d
momo1093324 <71679878+momo1093324@users.noreply.github.com>
2020-11-10 16:08:28 +0800

    腳色可動可眺與得分

---
+++ Unuty_ASUA_2D_Game/Assets/scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void NextLeve1(string nameLv)
+    {
+        SceneManager.LoadScene(nameLv);
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("介面");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
--- Unuty_ASUA_2D_Game/Assets/scripts/Player.cs
+++ Unuty_ASUA_2D_Game/Assets/scripts/Player.cs
@@ -1,16 +1,38 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     //欄位:儲存資料
-    public GameObject final1;
+    public GameObject final;
+
+    //道具數量文字
+    public Text textCount;
+    //數量
+    public int count;
 
     private void OnTriggerEnter2D(Collider2D collision)
-        //碰撞偵測條件
-        //1.兩者物件必須皆有碰撞器 collider2D  2.兩者其一需有一個剛體 Rigidboby2D 
+    //碰撞偵測條件
+    //1.兩者物件必須皆有碰撞器 collider2D 
+    //2.兩者其一需有一個剛體 Rigidboby2D 
+    // collision 儲存碰到物件的資訊
     {
-        print("遇到傳送奇遇時間");
-        final1.SetActive(true);      //啟動設定(是)
+        //if(條件){條件成立時會執行的程式區塊}
+        //如碰到物件名稱為傳送門...
+      
+        if (collision.name=="傳送門")
+        {
+           final.SetActive(true);      //啟動設定(是)
+        }
+        //如果碰到鑽石就吃掉(鑽石會消失)
+        if (collision.tag=="鑽石")
+        {
+            Destroy(collision.gameObject);
+            //遞增
+            count++;
+            //道具數量的文字="鑽石數量:"+數量
+            textCount.text = "鑽石數量:" + count;
+        }
     }
 
 }
